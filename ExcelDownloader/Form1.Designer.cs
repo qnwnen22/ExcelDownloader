07ba5bd53ca7f74ccc2a5d1fc0a43e4f6e449f63
moka <qnwnen0228@gmail.com>
2023-09-07 16:05:36 +0900

    초기 커밋.

---
+++ ExcelDownloader/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace ExcelDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.test2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem,
+            this.test2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.testToolStripMenuItem.Text = "Test";
+            this.testToolStripMenuItem.Click += new System.EventHandler(this.testToolStripMenuItem_Click);
+            // 
+            // test2ToolStripMenuItem
+            // 
+            this.test2ToolStripMenuItem.Name = "test2ToolStripMenuItem";
+            this.test2ToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.test2ToolStripMenuItem.Text = "Test2";
+            this.test2ToolStripMenuItem.Click += new System.EventHandler(this.test2ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem test2ToolStripMenuItem;
+    }
+}
+
---
+++ ExcelDownloader/Form1.cs
@@ -0,0 +1,342 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.testToolStripMenuItem.Enabled = false;
+            var json2 = File.ReadAllText(@"D:\uploadProduct_list.txt");
+            Task task = DownloadExcel(json2);
+            task.Start();
+        }
+
+        private void test2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private Task DownloadExcel(string json)
+        {
+            var resultAsync = new Task(() =>
+            {
+                var excelConvertor = new ExcelConvertor();
+                excelConvertor.DownloadExcel(json);
+                excelConvertor = null;
+                this.Invoke(new System.Action(() =>
+                {
+                    this.testToolStripMenuItem.Enabled = true;
+                    MessageBox.Show("작업이 완료되었습니다.");
+                }));
+            });
+            return resultAsync;
+        }
+    }
+
+
+    public class ExcelConvertor
+    {
+        private class Header
+        {
+            public int Row { get; set; }
+            public int Column { get; set; }
+            public string Value { get; set; }
+            public List<Header> Headers { get; set; } = new List<Header>();
+        }
+
+        private int width = 0;
+        private int height = 0;
+        private int nextHeight = 0;
+
+        private Excel.Application excelApp = null;
+        private Excel.Workbook wb = null;
+        private Excel.Worksheet ws = null;
+        private List<Header> Headers { get; set; } = new List<Header>();
+
+        private int GetMaxDepth(Header parent, JToken jToken, int depth = 1)
+        {
+            int result = depth;
+            switch (jToken.Type)
+            {
+                case JTokenType.Object:
+                    result++;
+                    JObject jObject = JObject.Parse(jToken.ToString());
+                    List<JProperty> properties = jObject.Properties().ToList();
+
+                    for (int i = 0; i < properties.Count; i++)
+                    {
+                        JProperty property = properties[i];
+
+                        if (property.Value.HasValues)
+                        {
+                            Header header = Add(property.Name, result, width);
+                            var getDepth1 = GetMaxDepth(header, property.Value, result);
+                            parent.Headers.Add(header);
+                            width++;
+
+                            if (result < getDepth1)
+                            {
+                                result = getDepth1;
+                            }
+                        }
+                        else
+                        {
+                            var getDepth1 = GetMaxDepth(parent, property.Value, result);
+                            Header header = Add(property.Name, getDepth1, width);
+                            parent.Headers.Add(header);
+                            width++;
+                            Console.WriteLine($"{property.Name} : {width} / {getDepth1}");
+
+                            if (result < getDepth1)
+                            {
+                                result = getDepth1;
+                            }
+                        }
+
+                        if (properties.Count - 1 == i)
+                        {
+                            width--;
+                        }
+
+                    }
+                    break;
+                case JTokenType.Array:
+                    int currentDepth = result;
+                    JToken[] array = jToken.ToArray();
+                    JToken first = array.First();
+                    var getDepth2 = GetMaxDepth(parent, first, currentDepth);
+                    if (result < getDepth2)
+                    {
+                        result = getDepth2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+        private Header Add(string value, int row, int column)
+        {
+            ws.Cells[row, column] = value;
+            return new Header
+            {
+                Value = value,
+                Row = row,
+                Column = column
+            };
+        }
+        private int Merge(Header header, int maxDepth, int column = 0)
+        {
+            Range range = null;
+            if (header.Headers.Count == 0)
+            {
+                range = ws.Range[ws.Cells[header.Row, header.Column], ws.Cells[maxDepth, header.Column]];
+
+            }
+            else
+            {
+                var newColumn = (header.Column + header.Headers.Count - 1);
+                if (column < newColumn)
+                {
+                    column = newColumn;
+                }
+
+                for (int i = 0; i < header.Headers.Count; i++)
+                {
+                    Header child = header.Headers[i];
+                    var addColumn = Merge(child, maxDepth, newColumn);
+
+                    if (newColumn < addColumn)
+                    {
+                        newColumn = addColumn;
+                    }
+                }
+                range = ws.Range[ws.Cells[header.Row, header.Column], ws.Cells[header.Row, newColumn]];
+            }
+            range.HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            range.Merge();
+            return column;
+        }
+        public void WriteHeader(JObject jObject)
+        {
+            List<JProperty> properties = jObject.Properties().ToList();
+            int maxDepth = 0;
+
+            for (int i = 0; i < properties.Count; i++)
+            {
+                try
+                {
+                    JProperty property = properties[i];
+                    width++;
+
+                    JToken jToken = property.Value;
+                    Header header = Add(property.Name, 1, width);
+                    Headers.Add(header);
+
+                    var depth = 1;
+                    if (jToken.HasValues)
+                    {
+                        depth = GetMaxDepth(header, jToken);
+                        if (maxDepth < depth)
+                        {
+                            maxDepth = depth;
+                        }
+                    }
+
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+
+            foreach (Header item in Headers)
+            {
+                Merge(item, maxDepth);
+            }
+
+            height = maxDepth + 1;
+        }
+
+        public int WriteBody(JToken jToken, int row, int column)
+        {
+            int result = 0;
+
+            switch (jToken.Type)
+            {
+                case JTokenType.Object:
+                    JObject jobj = (JObject)jToken;
+                    List<JProperty> jProperties = jobj.Properties().ToList();
+                    int appandColumn = 0;
+                    for (int i = 0; i < jProperties.Count; i++)
+                    {
+                        JProperty property = jProperties[i];
+                        appandColumn = column + i;
+                        appandColumn += WriteBody(property.Value, row, appandColumn);
+                    }
+                    result = appandColumn - column;
+                    break;
+                case JTokenType.Property:
+
+                    break;
+                case JTokenType.Array:
+                    List<JToken> values = jToken.Children().ToList();
+                    for (int i = 0; i < values.Count; i++)
+                    {
+                        int addRow = (height + i);
+                        if (nextHeight < addRow)
+                        {
+                            nextHeight = addRow;
+                        }
+                        JToken value = values[i];
+                        result = WriteBody(value, addRow, column);
+                    }
+                    break;
+                default:
+                    ws.Cells[row, column] = jToken.ToString();
+                    break;
+            }
+            return result;
+        }
+
+        public void DownloadExcel(string json)
+        {
+            excelApp = new Excel.Application();
+            wb = excelApp.Workbooks.Add();
+            ws = wb.Worksheets.get_Item(1) as Worksheet;
+
+            var token = JToken.Parse(json);
+
+            try
+            {
+                if (token is JArray)
+                {
+                    DownloadExcel(JArray.Parse(json));
+                }
+                else
+                {
+                    var jObject = JObject.Parse(json);
+                    WriteHeader(jObject);
+                    DownloadExcel(jObject);
+                }
+
+                string save = Directory.GetCurrentDirectory() + "\\test.xls";
+                // 엑셀파일 저장
+                wb.SaveAs(save, Excel.XlFileFormat.xlWorkbookNormal);
+                wb.Close(true);
+                excelApp.Quit();
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.Message);
+            }
+            finally
+            {
+                ReleaseExcelObject(ws);
+                ReleaseExcelObject(wb);
+                ReleaseExcelObject(excelApp);
+                GC.Collect();
+            }
+        }
+        private void DownloadExcel(JArray jArray)
+        {
+            for (int i = 0; i < jArray.Count; i++)
+            {
+                JToken jToken = jArray[i];
+                var jObject = (JObject)jToken;
+                if (i == 0)
+                {
+                    WriteHeader(jObject);
+                }
+                DownloadExcel(jObject);
+                height = nextHeight + 1;
+            }
+        }
+
+
+        private void DownloadExcel(JObject jObject)
+        {
+            List<JToken> tokens = jObject.Values().ToList();
+            int addValue = 1;
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                int res = WriteBody(tokens[i], height, i + addValue);
+                addValue += res;
+            }
+        }
+
+        private void ReleaseExcelObject(object obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                    Marshal.ReleaseComObject(obj);
+                    obj = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                throw ex;
+            }
+        }
+    }
+
+}
